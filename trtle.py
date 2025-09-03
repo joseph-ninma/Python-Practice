2099c5505ddb3c6c35e3f3a77ac7f048475b9ca8
Joseph-ninma <smartninma@gmail.com>
2025-09-03 06:07:37 +0100

    trtle

---
+++ trtle.py
@@ -0,0 +1,38 @@
+import turtle
+
+
+# t = turtle.Turtle()
+
+# for i in range(3):
+#     t.left(120)
+#     t.forward(100)
+#     t.forward(100)
+
+# turtle.done
+
+"""
+
+turtle.bgcolor("black")
+turtle.bgcolor("cyan")
+turtle.pensize(2)
+for i in range(12):
+    turtle.begin_fill()
+    turtle.circle(90, 150)
+    turtle.left(130)
+    turtle.forward(138)
+    turtle.end_fill()
+turtle.done
+
+
+
+turtle.bgcolor('black')
+t = turtle.Turtle()
+colors = ['red', 'dark red']
+for number in range(400):
+    t.forward(number+1)
+    t.right(89)
+    t.pencolor(colors[number%2])
+turtle.done
+
+"""
+
